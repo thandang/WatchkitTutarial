5e0d2f2659443cdc82143d1ba2d39ddd6d08f5bf
Than Dang <dtthan@apide.com>
2015-05-20 11:18:25 +0200

    Initial Commit

---
+++ TestWatchApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestWatchApp
+//
+//  Created by Dang Thanh Than on 5/20/15.
+//  Copyright (c) 2015 Than Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestWatchApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestWatchApp
+//
+//  Created by Dang Thanh Than on 5/20/15.
+//  Copyright (c) 2015 Than Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
